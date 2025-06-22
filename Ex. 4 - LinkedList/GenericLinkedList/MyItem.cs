b51e2b954d99679e92e61bf5fdf4461039c2d9cb
Eitan312 <eitanratner123@gmail.com>
2025-06-22 20:02:06 +0300

    solved question

---
+++ Ex. 4 - LinkedList/GenericLinkedList/MyItem.cs
@@ -0,0 +1,40 @@
+﻿using GenericLinkedList.Abstract;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection.Metadata.Ecma335;
+
+namespace GenericLinkedList
+{
+    public class MyItem<T> : IMyItem<T>
+    {
+        [NotNull]
+        private T _content;
+        [AllowNull]
+        private MyItem<T> _nextItem;
+
+        public IMyItem<T> NextItem { get => _nextItem; set => _nextItem = (MyItem<T>)value; }
+
+        public T Content => _content;
+
+        public MyItem(T content)
+        {
+            if (content == null) 
+            { 
+                throw new ArgumentNullException("content"); 
+            }
+
+            _content = content;
+        }
+
+        public MyItem(T content, MyItem<T> next) 
+        {
+            if (content == null)
+            {
+                throw new ArgumentNullException("content");
+            }
+
+            _content = content;
+            _nextItem = next;
+        }
+    }
+}
